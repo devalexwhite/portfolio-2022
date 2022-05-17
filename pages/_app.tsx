3addbc69f3565b0ceb45aa16fc4ac83651b48dda
Alex <alex.white@hey.com>
2022-05-17 10:28:52 -0400

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,26 @@ import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+const isProduction = process.env.NODE_ENV === "production";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <svg
